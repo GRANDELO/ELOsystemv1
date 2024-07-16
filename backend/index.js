b9ee2176d7bb5a280f975914329c03aea1facc4c
tee <tee@Kinyi.Kinyi>
2024-07-16 19:13:32 +0300

    elo updates from Tim

--- backend/index.js
+++ backend/index.js
@@ -14,15 +14,9 @@ const port = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => {
-    const mongoURI = process.env.MONGO_URI;
-    const mongoIP = mongoURI.substring(mongoURI.indexOf('@') + 1, mongoURI.lastIndexOf('/'));
-    console.error('MongoDB IP:', mongoIP);
-    console.error(mongoIP,'Cod not connect to MongoDB:', err);
+  .catch((err) => console.error('Could not connect to MongoDB', err));
 
-  });
-
-// Routes
+//routes
 app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
