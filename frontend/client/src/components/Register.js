02ee9e8e53f1a5ca98f7bc287f9218240298f11f
tee <tee@Kinyi.Kinyi>
2024-07-17 15:14:07 +0300

    elo updates from Tim

--- frontend/client/src/components/Register.js
+++ frontend/client/src/components/Register.js
@@ -46,7 +46,7 @@ const Register = () => {
         <div className="formsep">
           <span>
               <label>Full Name:</label>
-              <input type="text" name="fullName" placeholder="Enter your name" value={formData.fullName} onChange={handleChange} required />
+              <input type="text" name="fullName" placeholder="Enter your name" pattern="[a-zA-Z]{4,}" value={formData.fullName} onChange={handleChange} required />
 
               <label>Email:</label>
               <input type="email" name="email" placeholder="yourname@gmail.com" pattern="[a-zA-Z0-9_]{4,}@gmail\.com" title="Please enter a valid gmail address in formart grandelo@gmail.com" value={formData.email} onChange={handleChange} required />
