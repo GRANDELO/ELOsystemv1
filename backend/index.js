d82650963c1dee030aa48795694f50e3accc5a89
tee <tee@Kinyi.Kinyi>
2024-07-17 12:41:31 +0300

    elo updates from Tim

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,7 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(cors());
-app.use(express.json({ type: 'application/json; charset=utf-8' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 mongoose.set('strictQuery', false);
