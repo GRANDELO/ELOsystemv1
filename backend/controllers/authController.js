9e54a7d68844f84e84452d3767ee7f6adaa1973d
tee <tee@Kinyi.Kinyi>
2024-07-17 11:20:37 +0300

    elo updates from Tim

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -83,7 +83,7 @@ const login = async (req, res) => {
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid password' });
+      return res.status(401).json({ message: 'Invalid password ' + user +" "+ user.password});
     }
 
 
