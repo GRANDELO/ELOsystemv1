caf3faaa6887c3d5fdf7f9aca9576e61e0bf8bd4
tee <tee@Kinyi.Kinyi>
2024-07-17 11:13:21 +0300

    elo updates from Tim

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
-
+mongoose.set('strictQuery', false);
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
