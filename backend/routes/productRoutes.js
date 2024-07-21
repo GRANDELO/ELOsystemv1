abcf11ae92093491b6eb2d0a1a941998cca92998
sam njoro <samnjorokibandi@gmail.com>
2024-07-21 16:02:49 +0300

    changes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,12 @@
 // routes.js or app.js
 const express = require('express');
-const { getProduct, getProducts, postProduct, updateProduct } = require('../controllers/productController');
+const { getProduct, getProducts, postProduct, updateProduct, getImage } = require('../controllers/productController');
 const router = express.Router();
 
 router.post('/products', postProduct);
 router.get('/products', getProducts);
 router.get('/products/:id', getProduct);
 router.put('/products/:id', updateProduct);
+router.get('/files/:filename', getImage);
 
 module.exports = router;
