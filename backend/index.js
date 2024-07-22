304bfeb208c1ea45a81730be3f0f95e180a41cf6
tee <tee@Kinyi.Kinyi>
2024-07-22 13:05:37 +0300

    elo updates from Tim

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,18 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const http = require('http');
 const socketIo = require('socket.io');
-const multer = require('multer');
-const { GridFsStorage } = require('multer-gridfs-storage');
-const crypto = require('crypto');
-const path = require('path');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
+const { upload, gfs, conn } = require('./services/gridFsConfig');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+mongoose.set('strictQuery', true);
 
 // Connect to MongoDB
 mongoose
@@ -24,36 +22,10 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB', err));
 
-// GridFS storage configuration
-const storage = new GridFsStorage({
-  url: process.env.MONGO_URI,
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-const upload = multer({ storage });
-
-// Route to handle file upload
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ file: req.file });
-});
-
 // Middleware to add upload and gfs to req
 app.use((req, res, next) => {
   req.upload = upload;
+  req.gfs = gfs;
   next();
 });
 
@@ -61,12 +33,11 @@ app.use((req, res, next) => {
 app.use('/api/auth', authRoutes);
 app.use('/api', productRoutes);
 
-// Socket.IO setup
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: 'GET,POST,PUT,DELETE',
   }
 });
 
