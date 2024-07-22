7bf580ccc24e3f203b15608b4f88b3c37e8bde84
tee <tee@Kinyi.Kinyi>
2024-07-22 17:49:31 +0300

    elo updates from Tim

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,18 @@
 const express = require('express');
+const router = express.Router();
 const {
+  postProduct,
   getProduct,
   getProducts,
-  postProduct,
   updateProduct,
-  deleteProduct,
-  getImage,
+  deleteProduct
 } = require('../controllers/productController');
-const authenticateToken = require('../middleware/authenticateToken');
-const upload = require('../middleware/upload');
-const router = express.Router();
 
-const handleUpload = (req, res, next) => {
-  req.upload.single('image')(req, res, (err) => {
-    if (err) {
-      return res.status(500).json({ message: 'Error uploading file' });
-    }
-    next();
-  });
-};
-
-router.post('/products', handleUpload, upload.single('image'), postProduct);
-router.get('/products', getProducts);
+// Define routes
+router.post('/products', postProduct);
 router.get('/products/:id', getProduct);
-router.put('/products/:id', handleUpload, upload.single('image'), updateProduct);
+router.get('/products', getProducts);
+router.put('/products/:id', updateProduct);
 router.delete('/products/:id', deleteProduct);
-router.get('/files/:filename', getImage);
 
 module.exports = router;
