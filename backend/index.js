f2fc5f1337861752a1bf36a4bb80e739ec9e8c00
tee <tee@Kinyi.Kinyi>
2024-07-22 12:59:14 +0300

    elo updates from Tim

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,20 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const http = require('http');
 const socketIo = require('socket.io');
+const multer = require('multer');
+const { GridFsStorage } = require('multer-gridfs-storage');
+const crypto = require('crypto');
+const path = require('path');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
-const { upload, gfs, conn } = require('./services/gridFsConfig');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
-mongoose.set('strictQuery', true);
 
 // Connect to MongoDB
 mongoose
@@ -22,9 +24,10 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB', err));
 
-// Middleware to add upload and gfs to req
+// GridFS storage configuration
 const storage = new GridFsStorage({
-  url: mongoURI,
+  url: process.env.MONGO_URI,
+  options: { useNewUrlParser: true, useUnifiedTopology: true },
   file: (req, file) => {
     return new Promise((resolve, reject) => {
       crypto.randomBytes(16, (err, buf) => {
@@ -41,17 +44,16 @@ const storage = new GridFsStorage({
     });
   }
 });
-const uploadz = multer({ storage });
+const upload = multer({ storage });
 
-// Routes
-app.post('/upload', uploadz.single('file'), (req, res) => {
+// Route to handle file upload
+app.post('/upload', upload.single('file'), (req, res) => {
   res.json({ file: req.file });
 });
 
-
+// Middleware to add upload and gfs to req
 app.use((req, res, next) => {
   req.upload = upload;
-  req.gfs = gfs;
   next();
 });
 
@@ -59,11 +61,12 @@ app.use((req, res, next) => {
 app.use('/api/auth', authRoutes);
 app.use('/api', productRoutes);
 
+// Socket.IO setup
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: "*",
-    methods: 'GET,POST,PUT,DELETE',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }
 });
 
