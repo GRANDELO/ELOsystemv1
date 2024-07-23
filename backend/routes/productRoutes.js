bd4a7389dd1bc790af60b3dc0f61153ae9d82442
sam njoro <samnjorokibandi@gmail.com>
2024-07-23 21:04:44 +0300

    changes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const upload = require('../middleware/upload');
 
-router.post('/', upload.single('image'), productController.createProduct);
-router.put('/:id', upload.single('image'), productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
-router.get('/:id', productController.getProductById);
-router.get('/', productController.getAllProducts);
+router.post('/products', upload.single('image'), productController.createProduct);
+router.put('/products/:id', upload.single('image'), productController.updateProduct);
+router.delete('/products/:id', productController.deleteProduct);
+router.get('/products/:id', productController.getProductById);
+router.get('/products', productController.getAllProducts);
 
 module.exports = router;
