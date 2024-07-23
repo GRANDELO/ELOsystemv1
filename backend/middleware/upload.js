3487cda40655c30e0e81f9adc73f20a6550c0061
sam njoro <samnjorokibandi@gmail.com>
2024-07-23 23:01:16 +0300

    changes

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,11 +1,15 @@
-// upload.js
-
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+const uploadDirectory = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'uploads/'));
+    cb(null, uploadDirectory);
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
