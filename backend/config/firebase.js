cd2ad98ef6925a811e91b1e8668525b71cac51e4
sam njoro <samnjorokibandi@gmail.com>
2024-08-05 20:43:03 +0300

    changes

---
+++ backend/config/firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+const admin = require('firebase-admin');
+const { cert } = require('firebase-admin/app');
+const { getStorage } = require('firebase-admin/storage');
+
+const serviceAccount = require('./grandelo-firebase-adminsdk-lxsyl-d7518e4b02.json'); // Update this path
+
+admin.initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: 'grandelo.appspot.com' // Update with your Firebase project ID
+});
+
+const bucket = getStorage().bucket();
+
+module.exports = { bucket };
