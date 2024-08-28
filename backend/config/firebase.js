5f61ed1fd3be952b06ee96d10c078230b5593532
sam njoro <samnjorokibandi@gmail.com>
2024-08-29 00:21:26 +0300

    productform

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 const admin = require('firebase-admin');
 const { cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); // Update this path accordingly
+const serviceAccount = require('./grandelo-firebase-adminsdk-lxsyl-d7518e4b02.json') // Update this path accordingly
 
 admin.initializeApp({
   credential: cert(serviceAccount),
