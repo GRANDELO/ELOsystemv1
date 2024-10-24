2ba3bd1b01409493fbfcc2aa3e2281e85d38817d
tee <tee@Kinyi.Kinyi>
2024-10-24 11:25:51 +0300

    elo updates from Tim

--- backend/routes/routes.lipanampesa.js
+++ backend/routes/routes.lipanampesa.js
@@ -1,16 +1,11 @@
-import express from 'express';
-import {
-    confirmPayment,
-    initiateSTKPush,
-    stkPushCallback
-} from "../controllers/controllers.lipanampesa.js";
-const router = express.Router()
+const express = require('express');
+const { confirmPayment, initiateSTKPush, stkPushCallback } = require("../controllers/controllers.lipanampesa.js");
+const { accessToken } = require("../middlewares/middlewares.generateAccessToken.js");
 
+const router = express.Router();
 
-import { accessToken } from "../middlewares/middlewares.generateAccessToken.js";
+router.route('/stkPush').post(accessToken, initiateSTKPush);
+router.route('/stkPushCallback/:Order_ID').post(stkPushCallback);
+router.route('/confirmPayment/:CheckoutRequestID').post(accessToken, confirmPayment);
 
-router.route('/stkPush').post(accessToken,initiateSTKPush)
-router.route('/stkPushCallback/:Order_ID').post(stkPushCallback)
-router.route('/confirmPayment/:CheckoutRequestID').post(accessToken,confirmPayment)
-
-export default router
+module.exports = router;
