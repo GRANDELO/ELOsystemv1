c4720f3918d58cc9c9b674c3da344d105d82b8c1
tee <tee@Kinyi.Kinyi>
2024-10-25 13:58:03 +0300

    elo updates from Tim

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const { cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage');
 
-const serviceAccount = require('./grandelo-firebase-adminsdk-lxsyl-d7518e4b02.json') // Update this path accordingly
+const serviceAccount = require('./grandbucket-83f9f-firebase-adminsdk-un98y-8d45eadfd8.json') // Update this path accordingly
 
 admin.initializeApp({
   credential: cert(serviceAccount),
