c6f12ad66c5425b5fe9568a23934a943bd219c7a
tee <tee@Kinyi.Kinyi>
2024-10-27 22:53:26 +0300

    elo updates from Tim

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,8 +2,19 @@ const admin = require('firebase-admin');
 const { cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage');
 
-const serviceAccount = require('./grandelo-firebase-adminsdk-lxsyl-d7518e4b02.json') // Update this path accordingly
-
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.Universe_domain
+};
 admin.initializeApp({
   credential: cert(serviceAccount),
   storageBucket: 'grandelo.appspot.com' // Your Firebase storage bucket name
