b8a38c3e7f7327536164e1a4b0392eaf99401df8
tee <tee@Kinyi.Kinyi>
2024-10-28 20:08:45 +0300

    elo updates from Tim

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,8 +4,6 @@ const orderController = require('../controllers/orderController');
 
 // Routes for Orders
 router.post('/', orderController.createOrder);
-router.put('/assign', orderController.assignOrder); // Route to assign order
-router.get('/', orderController.getOrders); // Route to fetch all orders
 
 // You can add more routes here if needed.
 
