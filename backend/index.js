e350fc7f92162dbe2b8cf102fb4971e34d2b7793
tee <tee@Kinyi.Kinyi>
2024-11-15 00:08:33 +0300

    elo updates from Tim

--- backend/index.js
+++ backend/index.js
@@ -76,7 +76,7 @@ app.use(helmet({
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 1000,
+  max: 100,
   message: 'Too many requests from this IP, please try again after 15 minutes',
 });
 
