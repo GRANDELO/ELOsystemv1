d27171d569ceb4a7c1309120f0678697235df714
Sam njoro <samnjorokibandi@gmail.com>
2024-12-14 16:46:42 +0300

    el sam

--- backend/routes/feedRoutes.js
+++ backend/routes/feedRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { submitFeedback, getAllFeedback } = require('../controllers/feedbackController');
+const { submitFeedback, getAllFeedback } = require('../controllers/FeedControllers');
 
 
 router.post('/submit', submitFeedback);
