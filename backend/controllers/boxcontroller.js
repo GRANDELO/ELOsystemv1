75faa2378b0e27b67f6809139963aed4af595e6c
TK <tk@TEE.TEE>
2024-12-18 13:05:34 +0300

    elo updates from Tim

--- backend/controllers/boxcontroller.js
+++ backend/controllers/boxcontroller.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Box = require('../models/Box'); // Box model
+const Box = require('../models/box'); // Box model
 const User = require('../models/agents'); // User model with agent's info
 const { v4: uuidv4 } = require('uuid');
 
