f40a703e8f194807a10722abfc2ae3e3e33bb4f4
TK <tk@TEE.TEE>
2024-12-18 18:27:21 +0300

    elo updates from Tim

--- backend/routes/deliveryroutes.js
+++ backend/routes/deliveryroutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, login, verifyUser, updateEmail, resendVerificationCode, newrecoverPassword, resetPassword, changeusername, changepassword, changephonenumber, changeemail, logout} = require('../controllers/deliverypersoncontroller');
+const { registerUser, login, verifyUser, updateEmail, resendVerificationCode, newrecoverPassword, resetPassword,  changepassword, changephonenumber, changeemail, logout} = require('../controllers/deliverypersoncontroller');
 const router = express.Router();
 
 
@@ -10,7 +10,6 @@ router.post('/update-email', updateEmail);
 router.post('/resendemail', resendVerificationCode);
 router.post('/recoverpassword', newrecoverPassword);
 router.post('/reset-password', resetPassword);
-router.post('/changeusername', changeusername);
 router.post('/changepassword', changepassword);
 router.post('/changephonenumber', changephonenumber);
 router.post('/changeemail', changeemail);
