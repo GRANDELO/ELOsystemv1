43527493acbc3bf83385a3230a2f7bb6f0096a4c
TK <tk@TEE.TEE>
2024-12-28 16:50:26 +0300

    elo updates from Tim

--- backend/controllers/location.js
+++ backend/controllers/location.js
@@ -3,6 +3,7 @@ const Agent = require('../models/agents'); // Adjust the path to your model file
 const getAllLocations = async (req, res) => {
   try {
     const locations = await Agent.find({}, 'locations'); // Only fetch the 'locations' field
+    console.log(locations);
     res.status(200).json({ 
       success: true, 
       locations 
