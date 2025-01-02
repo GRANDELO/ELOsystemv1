1c38807b6f52bd6f2101af6d1d8b5422642c99eb
TK <tk@TEE.TEE>
2025-01-02 23:38:46 +0300

    elo updates from Tim

--- frontend/clientadmin/src/components/admin/accouns/TrialBalance.js
+++ frontend/clientadmin/src/components/admin/accouns/TrialBalance.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './TrialBalance.css'; // Import the CSS file for styling
+import '../styles/TrialBalance.css'; // Import the CSS file for styling
 
 const TrialBalance = () => {
   const [trialBalance, setTrialBalance] = useState([]);
