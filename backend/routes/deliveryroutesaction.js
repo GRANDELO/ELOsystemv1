8a81126b91cc64ee44502443b340fd0e5fe5b18a
TK <tk@TEE.TEE>
2025-01-31 23:40:11 +0300

    elo updates from Tim

--- backend/routes/deliveryroutesaction.js
+++ backend/routes/deliveryroutesaction.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/deliveryController');
-const { check } = require('express-validator');
 
 // Show all users
 router.get('/users', UserController.getAllUsers);
