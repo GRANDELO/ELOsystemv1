9a02125ace6085617a4a275659b99ec1aaa57d24
TK <tk@TEE.TEE>
2025-01-31 18:23:39 +0300

    elo updates from Tim

--- backend/routes/agentRoutes.js
+++ backend/routes/agentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/userController');
+const UserController = require('../controllers/agentController');
 
 // Show all users
 router.get('/users', UserController.getAllUsers);
