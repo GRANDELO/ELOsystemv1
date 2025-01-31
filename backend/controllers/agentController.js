3584b00e27dd64b083d729e895c850998e3002d9
TK <tk@TEE.TEE>
2025-01-31 19:18:34 +0300

    elo updates from Tim

--- backend/controllers/agentController.js
+++ backend/controllers/agentController.js
@@ -6,6 +6,7 @@ exports.getAllUsers = async (req, res) => {
   try {
     
     const users = await User.find();
+    console.log(users)
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching users', error });
