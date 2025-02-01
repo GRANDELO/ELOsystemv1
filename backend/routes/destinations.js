5a81c4126bf8ea12d41bef0d0f551e1bb0f272c7
Sam njoro <samnjorokibandi@gmail.com>
2025-02-01 10:20:02 +0300

    el sam

--- backend/routes/destinations.js
+++ backend/routes/destinations.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { groupProductsByOriginAndDestination } =  require('../controllers/location_pac');
 
-router.get('/destinations', groupProductsByOriginAndDestination);
+router.get('/destinations', async (req, res) => {
+    try {
+      const groupedProducts = await groupProductsByOriginAndDestination();
+      res.status(200).json({ data: groupedProducts });
+    } catch (error) {
+      console.error('Error fetching destinations:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 module.exports = router
