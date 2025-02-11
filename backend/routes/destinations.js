00daae2ec266b6f7c588f00de1a5294ef50d5ebb
Sam njoro <samnjorokibandi@gmail.com>
2025-02-11 13:09:42 +0300

    el sam

--- backend/routes/destinations.js
+++ backend/routes/destinations.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { planDeliveryLocations, groupProductsByOriginAndDestination, getDestinations } =  require('../controllers/location_pac');
 
-router.get('/destinations', getDestinations);
+//router.get('/destinations', getDestinations);
 
 router.post('/plan-delivery', planDeliveryLocations);
 
