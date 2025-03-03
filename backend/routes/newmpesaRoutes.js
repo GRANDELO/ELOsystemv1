9f3b8fb52ed9796563dd2581076ed8cf7bc74416
TK <tk@TEE.TEE>
2025-03-03 11:16:28 +0300

    elo updates from Tim

--- backend/routes/newmpesaRoutes.js
+++ backend/routes/newmpesaRoutes.js
@@ -4,6 +4,7 @@ const mpesaController = require("../controllers/mpesaController");
 
 router.get("/access_token", mpesaController.getAccessTokenHandler);
 router.get("/stkpush", mpesaController.stkPushHandler);
+router.post("/stkpush", mpesaController.stkPushHandler);
 router.post("/callback", mpesaController.stkPushCallbackHandler);
 router.get("/registerurl", mpesaController.registerURLHandler);
 router.get("/confirmation", mpesaController.confirmURLHandler);
@@ -12,4 +13,4 @@ router.get("/b2curlrequest", mpesaController.b2cRequestHandler);
 router.get("/b2c/queue", mpesaController.queue);
 router.get("/b2c/result", mpesaController.result);
 
-module.exports = router;
+module.exports = router; 
